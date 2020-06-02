3da52139664fad406d5a5862480319dae2a284e7
Sahil-1807 <66154865+Sahil-1807@users.noreply.github.com>
2020-06-02 18:49:02 +0530

    Create program5.py

---
+++ day4/program5.py
@@ -0,0 +1,20 @@
+n=int(input("enter number of votes:"))
+votes=[]
+for i in range(n):
+    votes.append(input("enter the candidate which you want to vote:"))
+votes.sort()
+cand={}
+for i in votes:
+    if i in cand:
+        cand[i]+=1
+    else:
+        cand[i]=1
+        
+max=0
+for i in cand:
+    if cand[i]>max:
+        max=cand[i]
+
+for i in cand:
+    if cand[i]==max:
+        print("maximum voter candidate:",i,"with votes:",max)
